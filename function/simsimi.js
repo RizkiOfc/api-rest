14cf2d81c9aca450cf9c364922b870feede6b3de
RizkiOfc <tururizki21@gmail.com>
2025-02-22 08:01:14 +0700

    Create simsimi.js

---
+++ function/simsimi.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+const { URLSearchParams } = require('url');
+ 
+async function simSimi(text, languageCode = 'id') {
+  const url = 'https://api.simsimi.vn/v1/simtalk';
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+  };
+ 
+  const data = new URLSearchParams();
+  data.append('text', text);
+  data.append('lc', languageCode);
+ 
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers,
+      body: data.toString(),
+    });
+    const rawResponse = await response.text();
+    console.log('Raw Response:', rawResponse);
+ 
+    // .text() => .json()
+    try {
+      const jsonResponse = JSON.parse(rawResponse);
+      return jsonResponse.message;
+    } catch (error) {
+      console.error('Respons bukan JSON:', rawResponse);
+      throw new Error('Respons dari server bukan JSON yang valid');
+    }
+  } catch (error) {
+    console.error('Error asking SimSimi:', error);
+    throw error;
+  }
+}
+
+module.exports = { simsimi }
