848001fca5d9b0cf6d81aeeed3617ccc8a9f065c
RizkiOfc <tururizki21@gmail.com>
2025-02-22 09:17:37 +0700

    Update facebook.js

--- function/facebook.js
+++ function/facebook.js
@@ -1,41 +1,14 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const FormData = require('form-data');
- 
+const getFbVideoInfo = require("fb-downloader-scrapper")
+require("../settings.js")
+
 async function fbdl(fbUrl) {
-  try {
-    const form = new FormData();
-    form.append('id', fbUrl);
-    form.append('locale', 'id');
- 
-    const response = await axios.post('https://getmyfb.com/process', form, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
- 
-    if (response.status === 200) {
-      const $ = cheerio.load(response.data);
-      const title = $('.results-item-text').text().trim();
-      const thumbnail = $('.results-item-image').attr('src');
-      const urlDownloads = {};
-      const urlHd = $('.results-list li:nth-child(1) a').attr('href');
-      const urlSd = $('.results-list li:nth-child(2) a').attr('href');
-      return {
-          title: title,
-          thumb: thumbnail,
-          video: {
-              hd: urlHd,
-              sd: urlSd,
-          }
-      }
-    } else {
-      return response.status;
+try {
+    const res = await getFbVideoInfo(fbUrl);
+    return res
+    } catch (error) {
+    return console.log(error)
     }
-  } catch (error) {
-    console.log('Error:', error);
-    return error;
-  }
-              }
+}
 
 module.exports = { fbdl }
+
