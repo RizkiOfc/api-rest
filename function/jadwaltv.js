04da8d7e18c4bbc3c36c0d8d693adb0f771149e3
RizkiOfc <tururizki21@gmail.com>
2025-02-22 18:59:30 +0700

    Update jadwaltv.js

--- function/jadwaltv.js
+++ function/jadwaltv.js
@@ -7,13 +7,14 @@ async function jadwaltv(channel) {
         const { data } = await axios.get(url);
         const $ = cheerio.load(data);
 
+        let res = [];
+
         $('table tbody tr').each((i, el) => {
             const jam = $(el).find('td').eq(0).text().trim();
             const acara = $(el).find('td').eq(1).text().trim();
-            const res = {
-                jam,
-                acara
-            };
+            if(!jam & acara) {
+                res.push({jam, acara})
+            }
             //console.log(acara)
             return res
         })
