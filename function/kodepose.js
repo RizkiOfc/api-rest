15918c24142d7b6198d62717565844d7dc419d10
RizkiOfc <tururizki21@gmail.com>
2025-02-25 18:44:44 +0700

    Create kodepose.js

---
+++ function/kodepose.js
@@ -0,0 +1,28 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+async function getKode(query) {
+    try {
+        const url = `https://nomorkodepos.com/?s=${encodeURIComponent(query)}`;
+        const { data } = await axios.get(url);
+        const $ = await cheerio.load(data);
+
+        let result = [];
+
+        $("table tbody tr").each((i, el) => {
+            const klm = $(el).find("td");
+            if(klm.length >= 3) {
+                const kelurahan = $(klm[0]).text().trim();
+                const kecamatan = $(klm[1]).text().trim();
+                const kode = $(klm[2]).text().trim();
+
+                result.push({ kelurahan, kecamatan, kode });
+            }
+            return result
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+modeule.exports = { getKode }
