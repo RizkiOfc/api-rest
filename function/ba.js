ee0d86008be2135d999812894bb3bd63e1f9479b
RizkiOfc <tururizki21@gmail.com>
2025-04-05 09:55:17 +0700

    Update ba.js

--- function/ba.js
+++ function/ba.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function blueArchive() {
+async function ba() {
     try {
         const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`);
         const response = await axios.get(data[Math.floor(data.length * Math.raqndom())], { responseType: 'arraybuffer' });
